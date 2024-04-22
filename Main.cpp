4a1fbdf243061c0722affcd9e970eb7c3bce902e
Kim-Chu-0x0 <151929419+Kim-Chu-0x0@users.noreply.github.com>
2024-04-23 01:21:12 +0800

    .

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+// for testing input module
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Control_Module_Main/Action_Process.h"
+Action_Processor R_Main;
+
+#include "Control_Module_Main/Data_Storage.h"
+Data_Storage D_Main;
+
+#include "Control_Module_Main/Input.h"
+Input I_Main;
+
+using namespace std;
+
+int main()
+{
+    srand(0);
+    I_Main.Initialize();
+    // D_Main.Test_Specific_Card(2);
+    for (int ff = 0; ff < 200; ff++)
+    {
+        if (ff>3){
+            D_Main.Test_Specific_Card(21);
+        }
+        I_Main.Read_input();
+        I_Main.Execute_Command();
+    }
+}
