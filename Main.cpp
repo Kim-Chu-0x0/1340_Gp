ff8e60db1a0543a92afe1f433b75731f744fddfb
cyx2015s <miracle041206@163.com>
2024-04-24 21:10:04 +0800

    Fix unicode character decoding

--- Main.cpp
+++ Main.cpp
@@ -13,8 +13,9 @@ Input I_Main;
 
 using namespace std;
 
-int main()
-{
+int main() {
+    // Set console code page to 65001 so program can use Unicode characters
+    system("chcp 65001");
     srand(0);
     I_Main.Initialize();
     //D_Main.Test_Specific_Card(12);
